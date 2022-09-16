b7aeb4f42152d90aa6382e1e3fb473ea8ae365fd
Anderson Pulz <67700882+pulz99@users.noreply.github.com>
2022-09-16 11:43:05 -0300

    Update pages.js

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -35,7 +35,7 @@ $(document).ready(function () {
 storefront.on('widget:@ecomplus/widget-minicart', function () {
     setTimeout(function () {
       if (storefront && storefront.context && storefront.context.body.name.indexOf('PRÉ-VENDA') > -1) {
-        document.querySelector('.product__prices').insertAdjacentHTML('afterend', '<div id="lancamento" style="color: #624175"> Envio em Outubro 2022! 🚚 </div>')
+        document.querySelector('.product__prices').insertAdjacentHTML('afterend', '<div id="lancamento" style="color: #624175"> **Envio em Outubro 2022! 🚚** </div>')
       }
     }, 2400);
   });
