e7060636847052b776d3bcd4df89bd571bbf2b7e
Leonardo Matos <leomp120894@gmail.com>
2023-08-14 21:09:56 -0300

    Update index.js

--- .functions/index.js
+++ .functions/index.js
@@ -1,7 +1,21 @@
 const functions = require('firebase-functions')
+const { onRequest } = require('firebase-functions/v2/https')
 
 const { ssr } = require('@ecomplus/storefront-renderer/functions/')
 
 process.env.STOREFRONT_LONG_CACHE = 'true'
 
 exports.ssr = functions.https.onRequest((req, res) => ssr(req, res))
+
+exports.ssr2 = onRequest({
+  concurrency: 80,
+  minInstances: 0,
+  memory: '1GiB',
+}, async (req, res) => {
+  if (req.path.startsWith('/~partytown')) {
+    res.sendStatus(404)
+    return null
+  }
+  res.set('x-load-took', '1')
+  return ssr(req, res)
+})
